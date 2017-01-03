e1384be20285c7b1b92b068549a8ade6f75b7cf2
Natenom <natenom@gmail.com>
2017-01-03 05:40:30 +0100

    Create install_desktop.sh

---
+++ install_desktop.sh
@@ -0,0 +1,7 @@
+sudo apt-get install xinit xfce4 sddm gmpc gmpc-plugins
+sudo systemctl enable sddm
+sudo systemctl restart sddm
+
+#autostart gmpc in foreground
+#autostart music folder in foreground
+#autostart log window in foreground
