6bd696f97ef73397d0eb958301b6278d44e24309
Natenom <natenom@gmail.com>
2017-01-03 06:01:14 +0100

    Update install_desktop.sh

--- install_desktop.sh
+++ install_desktop.sh
@@ -8,6 +8,10 @@ mkdir /home/botmaster/music/drop_your_music_here
 ln -s /home/botmaster/music/ /home/botmaster/Desktop/music_of_your_bot
 
 mkdir /home/botmaster/.config/gmpc/
+mkdir /home/botmaster/.autostart/
+
+# Enable autostart for gmpc
+cp /usr/share/applications/gmpc.desktop /home/botmaster/.config/autostart/
 
 # Create config for gmpc
 cat<<EOF > /home/botmaster/.config/gmpc/profiles.cfg
@@ -19,6 +23,21 @@ portnumber="7701"
 useauth="0"
 music directory="/home/botmaster/music/"
 db update time="1445647261"
+EOF
+
+# Create .desktop for bot live logging and write it to autostart
+cat<<EOF > /home/botmaster/.config/autostart/botlogging.desktop
+[Desktop Entry]
+Exec=/home/botmaster/src/mumble-ruby-pluginbot/scripts/manage.sh log
+GenericName=Watch bot logs live
+Icon=exec
+MimeType=
+Name=Watch bot logs live
+Path=
+StartupNotify=true
+Terminal=true
+TerminalOptions=
+Type=Application
 EOF
 
 #autostart gmpc in foreground
