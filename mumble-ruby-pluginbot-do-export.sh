fce385204ef68b52bad7635125fe63cec33b81f7
Natenom <natenom@gmail.com>
2017-01-07 15:25:16 +0100

    Update mumble-ruby-pluginbot-do-export.sh

--- mumble-ruby-pluginbot-do-export.sh
+++ mumble-ruby-pluginbot-do-export.sh
@@ -20,7 +20,7 @@ sudo fstrim -v /
 sudo fstrim -v /home
 history -c
 
-sudo find /var/ -iname "*.log" -type f -delete
+sudo find /var/ -iname "*log" -type f -delete
 
 
 echo "Export preparations done..."
